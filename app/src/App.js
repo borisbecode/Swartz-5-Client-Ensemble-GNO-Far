91b7b49fd61a966cbd1b86c96d8bd1aee4ddb338
Saelens Aurelien <aureliensaelens0@gmail.com>
2022-02-22 12:29:19 +0100

    update

--- app/src/App.js
+++ app/src/App.js
@@ -1,55 +1,21 @@
-import React, { Component } from 'react';
+import {Route, Routes, Navigate } from "react-router-dom";
+import React, { Component }  from 'react';
+import Signup from "./components/Signup/Signup";
+import Login from "./components/Login/Login";
+import Main from "./components/Main/Main";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
+function App() {
+	const user = localStorage.getItem("token");
 
-  componentDidMount() {
-    fetch("http://192.168.33.10:5000/getjson")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
+	return (
+		<Routes>
+			{user && <Route path="/" exact element={<Main />} />}
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
 
-  render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <ul>
-          {items.map(item => (
-            <li key={item.first_name}>
-              {item.first_name} {item.last_name}
-            </li>
-          ))}
-        </ul>
-      );
-    }
-  }
+	);
 }
 
 export default App;
