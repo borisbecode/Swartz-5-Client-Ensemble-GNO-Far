eca15db0ca010915f374d4d1444e6c6dd159ef05
borisbecode <boris.lorenzonetto@gmail.com>
2022-02-23 12:51:36 +0100

    ok

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 import About from "./pages/about";
 import Actions from "./pages/actions";
 import Admin from "./pages/admin";
@@ -10,26 +5,30 @@ import Home from "./pages/home";
 import NotFound from "./pages/notfound";
 import Parrainage from "./pages/parrainage";
 
-
-import {Route, Routes, Navigate } from "react-router-dom";
-import React, { Component }  from 'react';
+import { Route, Routes, Navigate } from "react-router-dom";
+import React, { Component } from "react";
 import Signup from "./components/Signup/Signup";
 import Login from "./components/Login/Login";
 import Main from "./components/Main/Main";
 
 function App() {
-	const user = localStorage.getItem("token");
+  const user = localStorage.getItem("token");
 
-	return (
-		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}
-			<Route path="/signup" exact element={<Signup />} />
-			<Route path="/login" exact element={<Login />} />
-			<Route path="/" element={<Navigate replace to="/login" />} />
-		</Routes>
+  return (
+    <Routes>
+      {user && <Route path="/" exact element={<Main />} />}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/home" exact element={<Home />} />
+      <Route path="/Admin" exact element={<Admin />} />
+      <Route path="/Notfound" exact element={<NotFound />} />
+      <Route path="/actions" exact element={<Actions />} />
+      <Route path="/About" exact element={<About />} />
+      <Route path="/Parrainage" exact element={<Parrainage />} />
 
-	);
-
+      <Route path="/" element={<Navigate replace to="/login" />} />
+    </Routes>
+  );
 }
 
 export default App;
