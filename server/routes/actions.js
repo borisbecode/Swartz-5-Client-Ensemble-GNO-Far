08e87b340b42515fa48130b89d3da0e494175493
Gysen Lionel <gysenlionel@gmail.com>
2022-02-24 20:43:36 +0100

    upload photo

--- server/routes/actions.js
+++ server/routes/actions.js
@@ -3,9 +3,21 @@ const router = express.Router()
 const multer = require("multer")
 const Actions = require("../models/actions")
 
+// definis le storage pour l'image
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "../app/public/uploads/")
+    },
+    filename: (req, file, callback) => {
+        callback(null, Date.now() + file.originalname)
+    }
+})
+// param pour multer
+const upload = multer({ storage: storage })
+
 // Get all acions
-router.get("/actions", (req, res) => {
-    Articles.find()
+router.get("/", (req, res) => {
+    Actions.find()
         .then((action) => {
             res.json(action)
         }).catch((err) => {
@@ -13,4 +25,52 @@ router.get("/actions", (req, res) => {
         });
 })
 
+// Add new action
+router.post('/add', upload.single("image"), (req, res) => {
+    const newAction = new Actions({
+        title: req.body.title,
+        content: req.body.content,
+        location: req.body.location,
+        link: req.body.location,
+        image: req.file.filename
+    })
 
+    newAction.save()
+        .then(() => res.json('Nouvelle action ajoutée'))
+        .catch(err => res.status(400).json(`Error: ${err}`))
+})
+
+// Find action by id
+router.get('/:id', (req, res) => {
+    Actions.findById(req.params.id)
+        .then(action => res.json(action))
+        .catch(err => res.status(400).json(`Error: ${err}`))
+})
+
+// Find action by id and update
+router.put('/update/:id', upload.single("image"), (req, res) => {
+    Actions.findById(req.params.id)
+        .then(action => {
+            action.title = req.body.title
+            action.content = req.body.content
+            action.location = req.body.location
+            action.link = req.body.link
+            action.image = req.file.filename
+
+            action
+                .save()
+                .then(() => res.json("L'action est mise à jour"))
+                .catch(err => res.status(400).json(`Error: ${err}`))
+        })
+        .catch(err => res.status(400).json(`Error: ${err}`))
+})
+
+// Find action by id and delete
+router.delete('/:id', (req, res) => {
+    Actions.findByIdAndDelete(req.params.id)
+        .then(() => res.json("L'action est supprimée"))
+        .catch(err => res.status(400).json(`Error: ${err}`))
+})
+
+
+module.exports = router
