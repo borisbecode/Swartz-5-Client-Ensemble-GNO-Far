839f16897a772b8edfaadefcdd280b2daa3f952f
Saelens Aurelien <aureliensaelens0@gmail.com>
2022-02-24 21:09:45 +0100

    add router

--- server/index.js
+++ server/index.js
@@ -23,5 +23,5 @@ app.use("/api/articles", articlesRoutes);
 
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...🤓🤓🤓🤓🤓🤓🤓`));
 
