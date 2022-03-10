18c3f42d793736d56d8c7d7cd6f3bc996f433e63
borisbecode <boris.lorenzonetto@gmail.com>
2022-03-10 11:20:58 +0100

    ok

--- app/src/pages/Accueil.js
+++ app/src/pages/Accueil.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import BannerHome from '../components/BannerHome';
-import Articles from '../components/Articles';
+import React from 'react'
+import Box from '@mui/material/Box'
+import BannerHome from '../components/BannerHome'
+import Articles from '../components/Articles'
+import Footer from '../components/Footer/Footer'
 
 const Accueil = () => {
   return (
-    <Box id="accueil" sx={{width:"100%", m:0}}>
-        <BannerHome />
-        <Articles />
-
+    <Box id="accueil" sx={{ width: '100%', m: 0 }}>
+      <BannerHome />
+      <Articles />
+      <Footer />
     </Box>
   )
 }
