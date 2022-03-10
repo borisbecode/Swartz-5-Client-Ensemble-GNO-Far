e077f637f4cc611ee114365e05a06eb9833d7b37
ClaraCliment <climent.clara@gmail.com>
2022-03-10 11:03:17 +0100

    Petits changements App.js & Parrainage.js

--- app/src/App.js
+++ app/src/App.js
@@ -4,13 +4,13 @@ import Admin from "./pages/admin";
 import NotFound from "./pages/notfound";
 import React from "react";
 import './App.css';
-import Parrainage from "./pages/parrainage";
 import { Route, Routes, Navigate } from "react-router-dom";
 // import Signup from "./components/Signup/Signup";
 import Login from "./components/Login/Login";
 import Box from '@mui/material/Box';
 import Header from "./components/Header/Header";
 import Accueil from "./pages/Accueil";
+import Parrainage from './pages/Parrainage';
 
 
 function App() {
@@ -21,12 +21,12 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" exact element={<Accueil />} />
+          <Route path="/actions" exact element={<Actions />} />
+          <Route path="/parrainage" exact element={<Parrainage />} />
+          <Route path="/quisommesnous" exact element={<About />} />
           <Route path="/connexion" exact element={<Login />} />
           <Route path="/admin" exact element={<Admin />} />
           <Route path="/introuvable" exact element={<NotFound />} />
-          <Route path="/actions" exact element={<Actions />} />
-          <Route path="/quisommesnous" exact element={<About />} />
-          <Route path="/parrainage" exact element={<Parrainage />} />
         </Routes>
     </Box>
   );
---
+++ app/src/pages/Parrainage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+const Parrainage = () => {
+  return (
+    <Box 
+    sx={{width:"100%", height:"100vh", m:0, bgcolor:"primary.main"}}
+     >Blah</Box>
+  )
+}
+
+export default Parrainage
