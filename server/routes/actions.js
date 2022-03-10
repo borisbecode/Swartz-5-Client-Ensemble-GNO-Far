e2c8c477be60c2d4863ee75d9d996633df02f74c
Gysen Lionel <gysenlionel@gmail.com>
2022-03-10 15:56:48 +0100

    fr error format

--- server/routes/actions.js
+++ server/routes/actions.js
@@ -20,7 +20,7 @@ const upload = multer({
             cb(null, true)
         } else {
             cb(null, false);
-            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+            return cb(new Error('Seul les formats .png, .jpg and .jpeg sont admis!'));
         }
     },
     limits: {
