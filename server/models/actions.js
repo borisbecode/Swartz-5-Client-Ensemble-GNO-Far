6fbfc4897e0798af1a16ec61d032aaa59338945a
Gysen Lionel <gysenlionel@gmail.com>
2022-03-10 11:12:09 +0100

    model routes Action

--- server/models/actions.js
+++ server/models/actions.js
@@ -2,11 +2,18 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const actionsSchema = new Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
+    title: { type: String, required: true, trim: true },
+    content: { type: String, required: true, trim: true, },
     location: { type: String, required: false },
     link: { type: String, required: false },
-    image: { type: String, required: false }
+    image: { type: String, required: false },
+    status: {
+        type: String, enum: ['created', 'published', 'draft', 'deleted'], default:
+            'draft'
+    },
+    isDeleted: { type: Boolean, default: false },
+    createdAt: { type: Date },
+    updatedAt: { type: Date }
 })
 
 const Actions = mongoose.model("Actions", actionsSchema)
