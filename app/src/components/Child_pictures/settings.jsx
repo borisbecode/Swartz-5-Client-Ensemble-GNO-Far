e6be2726b6cef4dcf427f6a088f0512342e95e58
borisbecode <boris.lorenzonetto@gmail.com>
2022-03-14 14:09:52 +0100

    repo

--- app/src/components/Child_pictures/settings.jsx
+++ app/src/components/Child_pictures/settings.jsx
@@ -1,26 +1,35 @@
 import { Box } from '@mui/material/node_modules/@mui/system'
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import axios from 'axios'
+import TextField from '@mui/material/TextField'
 
 class Settings extends React.Component {
   state = {
     Lefichier: null,
+    NomDeLenfant: null,
   }
+
   fichierAEnvoyer = (event) => {
     this.setState({
       Lefichier: event.target.files[0],
     })
   }
 
+  Nomdelenfant = (event) => {
+    this.setState({
+      NomDeLenfant: event.target.values,
+    })
+  }
+
   EnvoiDuFichier = () => {
     const formulaire = new FormData()
     formulaire.append('image', this.state.Lefichier, this.state.Lefichier.name)
-    formData.append('title', titre)
+    formulaire.append('childName', this.state.NomDeLenfant)
 
     axios
-      .post(`http://localhost:5000/...`, formulaire)
+      .post(`http://localhost:5000/api/parraines/add`, formulaire)
       .then((res) => {
         console.log(res)
       })
@@ -46,6 +55,7 @@ class Settings extends React.Component {
         >
           <Typography> Choisir une image </Typography>{' '}
         </Button>
+        <input onChange={this.Nomdelenfant} />{' '}
         <Button
           onClick={this.EnvoiDuFichier}
           variant="contained"
