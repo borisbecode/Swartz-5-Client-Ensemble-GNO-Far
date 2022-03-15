4c8e2426fdd125feed2c80bcab570b0cf0369f26
borisbecode <boris.lorenzonetto@gmail.com>
2022-03-15 22:03:15 +0100

    t16

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connection = require("./server/db");
+const userRoutes = require("./server/routes/users");
+const authRoutes = require("./server/routes/auth");
+const actionRoutes = require("./server/routes/actions");
+const articlesRoutes = require("./server/routes/articles");
+const parrainesRoutes = require("./server/routes/parraines");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const Articles = require("./server/models/articles");
+/* const path = require('path') */
+
+require("dotenv").config();
+
+// DATABASE CONNECTION
+connection();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/actions", actionRoutes);
+app.use("/api/articles", articlesRoutes);
+app.use("/api/parraines", parrainesRoutes);
+
+/* if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'app/build')))
+  app.get('*', function (req, res) {
+    res.status(200).sendFile(path.join(__dirname, 'app/build', 'index.html'))
+  })
+} */
+
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "app/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "app/build", "index.html"));
+});
+
+/* if (process.env.NODE_ENV === 'production') {
+  app.use(express.static("app/build"))
+  const path = require('path')
+  app.get('*', function (req, res) {
+    res.status(200).sendFile(path.resolve(__dirname,"app","build","index.html"))
+  })
+ */
+/* const swaggerSpec = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec)); */
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Listening on PORT ${PORT}...🤓🤓🤓🤓🤓🤓🤓`)
+);
