7560301be8ffd9cc6b33f07f287a036796201d6c
borisbecode <boris.lorenzonetto@gmail.com>
2022-03-15 11:39:03 +0100

    ok

--- server/index.js
+++ server/index.js
@@ -1,49 +1,46 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const express = require('express')
+const app = express()
+const cors = require('cors')
 const connection = require('./db')
-const userRoutes = require('./routes/users');
-const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users')
+const authRoutes = require('./routes/auth')
 const actionRoutes = require('./routes/actions')
-const articlesRoutes = require('./routes/articles');
+const articlesRoutes = require('./routes/articles')
 const parrainesRoutes = require('./routes/parraines')
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const Articles = require('../server/models/articles');
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+const Articles = require('../server/models/articles')
 
-require("dotenv").config();
+require('dotenv').config()
 
-
-
-// DATABASE CONNECTION 
-connection();
+// DATABASE CONNECTION
+connection()
 
 // MIDDLEWARE
-app.use(cors());
+app.use(cors())
 app.use(express.json())
 
-// ROUTES 
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/actions", actionRoutes);
-app.use("/api/articles", articlesRoutes);
-app.use("/api/parraines", parrainesRoutes);
-
+// ROUTES
+app.use('/api/users', userRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/actions', actionRoutes)
+app.use('/api/articles', articlesRoutes)
+app.use('/api/parraines', parrainesRoutes)
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'node JS API Project for mongoDB',
-            version: '1.0.0'
-        },
-        servers: [
-            {
-                url: 'http://localhost:5000'
-            }
-        ]
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'node JS API Project for mongoDB',
+      version: '1.0.0',
     },
-    apis: ['./index.js']
+    servers: [
+      {
+        url: 'http://localhost:5000',
+      },
+    ],
+  },
+  apis: ['./index.js'],
 }
 
 //PARAMETRE TO GET ALL ARTICLES WITH SWAGGER
@@ -51,28 +48,26 @@ const options = {
  * @swagger
  *  /:
  *      get:
- *          summary: 
+ *          summary:
  *          description: This Api is use to check if get method is working or not.
  *          responses:
  *              200:
  *                  description: To test get Method
  */
 app.get('/', (req, res) => {
-    Articles.find()
-        .then(article => res.json(article))
-        .catch(error => res.status(400).json(`Error ${error}`))
-});
+  Articles.find()
+    .then((article) => res.json(article))
+    .catch((error) => res.status(400).json(`Error ${error}`))
+})
 
-
-
-
-
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('app/build'))
+}
 
 const swaggerSpec = swaggerJSDoc(options)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port ${port}...🤓🤓🤓🤓🤓🤓🤓`));
-
+const port = process.env.PORT || 5000
+app.listen(port, () =>
+  console.log(`Listening on port ${port}...🤓🤓🤓🤓🤓🤓🤓`)
+)
