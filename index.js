d52dbc16df252e6bc68fe3a4100962b2e32425b0
borisbecode <boris.lorenzonetto@gmail.com>
2022-03-15 16:11:22 +0100

    coucou8

--- index.js
+++ index.js
@@ -28,39 +28,6 @@ app.use("/api/actions", actionRoutes);
 app.use("/api/articles", articlesRoutes);
 app.use("/api/parraines", parrainesRoutes);
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "node JS API Project for mongoDB",
-      version: "1.0.0",
-    },
-    servers: [
-      {
-        url: process.env.CLIENT_URL,
-      },
-    ],
-  },
-  apis: ["./index.js"],
-};
-
-//PARAMETRE TO GET ALL ARTICLES WITH SWAGGER
-/**
- * @swagger
- *  /:
- *      get:
- *          summary:
- *          description: This Api is use to check if get method is working or not.
- *          responses:
- *              200:
- *                  description: To test get Method
- */
-app.get("/", (req, res) => {
-  Articles.find()
-    .then((article) => res.json(article))
-    .catch((error) => res.status(400).json(`Error ${error}`));
-});
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "app/build")));
   app.get("*", function (req, res) {
@@ -68,8 +35,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const swaggerSpec = swaggerJSDoc(options);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+/* const swaggerSpec = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec)); */
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
