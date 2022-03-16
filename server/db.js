08079ecacbbe73bae307a8b9f454f04f728cba33
borisbecode <boris.lorenzonetto@gmail.com>
2022-03-16 13:12:31 +0100

    ok

--- server/db.js
+++ server/db.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 module.exports = () => {
-    const connectionParams = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
-    try {
-        mongoose.connect(process.env.MONGO_URL, connectionParams)
-        console.log("connected to mongoDB!")
-    } catch (error) {
-        console.log(error);
-        console.log("There is a problem somewhere!")
-    }
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+  try {
+    mongoose.connect(process.env.MONGO_URL, connectionParams)
+    console.log('connected to mongoDB!')
+  } catch (error) {
+    console.log(error)
+    console.log('There is a problem somewhere!')
+  }
 }
