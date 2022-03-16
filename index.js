8f4760e1d855833852f8e2356e571ec5b43421da
borisbecode <boris.lorenzonetto@gmail.com>
2022-03-16 09:31:13 +0100

    ok

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const connection = require('./db')
-const userRoutes = require('./routes/users')
-const authRoutes = require('./routes/auth')
-const actionRoutes = require('./routes/actions')
-const articlesRoutes = require('./routes/articles')
-const parrainesRoutes = require('./routes/parraines')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connection = require("./server/db");
+const userRoutes = require("./server/routes/users");
+const authRoutes = require("./server/routes/auth");
+const actionRoutes = require("./server/routes/actions");
+const articlesRoutes = require("./server/routes/articles");
+const parrainesRoutes = require("./server/routes/parraines");
 /* const swaggerJSDoc = require('swagger-jsdoc')
 const swaggerUi = require('swagger-ui-express') */
-const Articles = require('./models/articles')
-const path = require('path')
-/* require('dotenv').config({ path: './server/.env' }) */
+const Articles = require("./server/models/articles");
+const path = require("path");
+require("dotenv").config({ path: "./server/.env" });
 /* require("./server/db"); */
 
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose");
+/* require("dotenv").config(); */
 
 /* module.exports = () => {
   const connectionParams = {
@@ -32,24 +32,24 @@ require('dotenv').config()
 }; */
 
 // DATABASE CONNECTION
-connection()
+connection();
 
 // MIDDLEWARE
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // ROUTES
-app.use('/api/users', userRoutes)
-app.use('/api/auth', authRoutes)
-app.use('/api/actions', actionRoutes)
-app.use('/api/articles', articlesRoutes)
-app.use('/api/parraines', parrainesRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/actions", actionRoutes);
+app.use("/api/articles", articlesRoutes);
+app.use("/api/parraines", parrainesRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'app/build')))
-  app.get('*', function (req, res) {
-    res.status(200).sendFile(path.join(__dirname, 'app/build', 'index.html'))
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "app/build")));
+  app.get("*", function (req, res) {
+    res.status(200).sendFile(path.join(__dirname, "app/build", "index.html"));
+  });
 }
 
 /* if (process.env.NODE_ENV === 'production') {
@@ -67,7 +67,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec)); */
   console.log(`Express is working on port ${port}`);
 }); */
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Listening on PORT ${PORT}...🤓🤓🤓🤓🤓🤓🤓`)
-)
+);
