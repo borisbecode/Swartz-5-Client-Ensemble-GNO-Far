73c226f3cdb275d553b1848fc9712af968332536
borisbecode <boris.lorenzonetto@gmail.com>
2022-03-16 15:32:37 +0100

    goodproduction

--- server/routes/parraines.js
+++ server/routes/parraines.js
@@ -1,77 +1,94 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const multer = require("multer")
-const Parraines = require("../models/parraines")
+const multer = require('multer')
+const Parraines = require('../models/parraines')
 
 // definis le storage pour l'image
 const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, '../app/public/uploads/')
+  },
+  filename: (req, file, callback) => {
+    callback(null, Date.now() + file.originalname)
+  },
+})
+
+if (process.env.NODE_ENV === 'production') {
+  const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-        callback(null, "../app/public/uploads/")
+      callback(null, 'app/build/uploads/')
     },
     filename: (req, file, callback) => {
-        callback(null, Date.now() + file.originalname)
-    }
-})
+      callback(null, Date.now() + file.originalname)
+    },
+  })
+}
 // param pour multer
 const upload = multer({
-    storage: storage,
-    fileFilter: function (req, file, cb) {
-        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-            cb(null, true)
-        } else {
-            cb(null, false);
-            return cb(new Error('Seul les formats .png, .jpg and .jpeg sont admis!'));
-        }
-        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-            cb(null, true)
-        } else {
-            cb(null, false);
-            return cb(new Error('Seul les formats .png, .jpg and .jpeg sont admis!'));
-        }
-    },
-    limits: {
-        fileSize: 1024 * 1024 * 3
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    if (
+      file.mimetype == 'image/png' ||
+      file.mimetype == 'image/jpg' ||
+      file.mimetype == 'image/jpeg'
+    ) {
+      cb(null, true)
+    } else {
+      cb(null, false)
+      return cb(new Error('Seul les formats .png, .jpg and .jpeg sont admis!'))
     }
-
+    if (
+      file.mimetype == 'image/png' ||
+      file.mimetype == 'image/jpg' ||
+      file.mimetype == 'image/jpeg'
+    ) {
+      cb(null, true)
+    } else {
+      cb(null, false)
+      return cb(new Error('Seul les formats .png, .jpg and .jpeg sont admis!'))
+    }
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 3,
+  },
 })
-
 
 // Get all childs
-router.get("/", (req, res) => {
-    Parraines.find()
-        .then((action) => {
-            res.json(action)
-        }).catch((err) => {
-            res.status(400).json(`Error: ${err}`)
-        });
+router.get('/', (req, res) => {
+  Parraines.find()
+    .then((action) => {
+      res.json(action)
+    })
+    .catch((err) => {
+      res.status(400).json(`Error: ${err}`)
+    })
 })
 
+// Add new pics
+router.post('/add', upload.single('image'), (req, res) => {
+  const IS_PUBLISHED = 'published'
 
-// Add new pics 
-router.post('/add', upload.single("image"), (req, res) => {
+  let parraine = {
+    childName: req.body.childName,
+    status: IS_PUBLISHED,
+    isDeleted: req.body.isDeleted,
+    // image: req.file.filename
+  }
+  // pour que l'image ne soit pas required
+  // if (req.file && req.file.filename) action.image = req.file.filename
 
-    const IS_PUBLISHED = 'published'
+  if (req.file.filename === 'undefined') {
+    return new Error('Not Found')
+  } else {
+    parraine.image = req.file.filename
+  }
 
-    let parraine = {
-        childName: req.body.childName,
-        status: IS_PUBLISHED,
-        isDeleted: req.body.isDeleted,
-        // image: req.file.filename
-    }
-    // pour que l'image ne soit pas required
-    // if (req.file && req.file.filename) action.image = req.file.filename
+  const newParraine = new Parraines(parraine)
 
-    if (req.file.filename === 'undefined') {
-        return new Error('Not Found')
-    } else {
-        parraine.image = req.file.filename
-    }
-
-    const newParraine = new Parraines(parraine)
-
-    newParraine.save()
-        .then(() => res.json('Nouvelle photo ajoutée'))
-        .catch(err => res.status(400).json(`Error: ${err}`))
+  newParraine
+    .save()
+    .then(() => res.json('Nouvelle photo ajoutée'))
+    .catch((err) => res.status(400).json(`Error: ${err}`))
 })
 
 module.exports = router
