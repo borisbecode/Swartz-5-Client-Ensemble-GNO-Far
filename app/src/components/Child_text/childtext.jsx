ffd0f9250f7bcb619fff49ad7a7fe9056ca65b63
borisbecode <boris.lorenzonetto@gmail.com>
2022-04-05 16:16:43 +0200

    ok

--- app/src/components/Child_text/childtext.jsx
+++ app/src/components/Child_text/childtext.jsx
@@ -24,12 +24,12 @@ function Childtext() {
       <Grid item xs={12} lg={10}>
         {' '}
         <Typography sx={{ mt: 2 }}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-          molestias architecto voluptate aliquam
+          ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+          molestiae quas vel sint commodi repudiandae consequuntur voluptatum
+          laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto
+          fuga praesentium optio, eaque rerum! Provident similique accusantium
+          nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias
+          architecto voluptate aliquam
         </Typography>
       </Grid>
       <Grid
