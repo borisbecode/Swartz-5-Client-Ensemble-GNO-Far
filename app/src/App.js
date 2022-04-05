0fa89a73fca29d2f4c65d4fe491d300576d602c6
Gysen Lionel <gysenlionel@gmail.com>
2022-04-05 10:53:37 +0200

    test log avec old files d aurelien

--- app/src/App.js
+++ app/src/App.js
@@ -11,12 +11,9 @@ import Header from './components/Header/Header'
 import Accueil from './pages/Accueil'
 import Parrainage from './pages/Parrainage'
 import Footer from './components/Footer/Footer'
-<<<<<<< HEAD
 import Signup from './components/Signup/Signup'
 
-=======
 import Login from './components/Login/Login'
->>>>>>> 6f308f1357d6d769c283495ff91c441c4d815c7b
 
 function App() {
   const user = localStorage.getItem('token')
