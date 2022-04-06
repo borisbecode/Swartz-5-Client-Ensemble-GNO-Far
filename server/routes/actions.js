b743909fc101d4ea2877177eefcc8011e5626b09
borisbecode <boris.lorenzonetto@gmail.com>
2022-04-06 17:30:08 +0200

    lio

--- server/routes/actions.js
+++ server/routes/actions.js
@@ -1,47 +1,51 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const multer = require("multer")
-const Actions = require("../models/actions")
+const multer = require('multer')
+const Actions = require('../models/actions')
 const checkAuth = require('../utils/checkAuth')
 
 // definis le storage pour l'image
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, "app/public/uploads/")
+    callback(null, 'app/build/uploads/')
   },
   filename: (req, file, callback) => {
     callback(null, Date.now() + file.originalname)
-  }
+  },
 })
 // param pour multer
 const upload = multer({
   storage: storage,
   fileFilter: function (req, file, cb) {
-    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+    if (
+      file.mimetype == 'image/png' ||
+      file.mimetype == 'image/jpg' ||
+      file.mimetype == 'image/jpeg'
+    ) {
       cb(null, true)
     } else {
-      cb(null, false);
-      return cb(new Error('Seul les formats .png, .jpg and .jpeg sont admis!'));
+      cb(null, false)
+      return cb(new Error('Seul les formats .png, .jpg and .jpeg sont admis!'))
     }
   },
   limits: {
-    fileSize: 1024 * 1024 * 3
-  }
-
+    fileSize: 1024 * 1024 * 3,
+  },
 })
 
 // Get all actions
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   Actions.find()
     .then((action) => {
       res.json(action)
-    }).catch((err) => {
+    })
+    .catch((err) => {
       res.status(400).json(`Error: ${err}`)
-    });
+    })
 })
 
 // Get  actions  delete
-router.get("/inactif", (req, res) => {
+router.get('/inactif', (req, res) => {
   Actions.find({ isDeleted: true }, function (err, action) {
     if (err) res.status(400).json(`Error: ${err}`)
     res.json(action)
@@ -49,7 +53,7 @@ router.get("/inactif", (req, res) => {
 })
 
 // Get  actions  not delete
-router.get("/actif", (req, res) => {
+router.get('/actif', (req, res) => {
   Actions.find({ isDeleted: false }, function (err, action) {
     if (err) res.status(400).json(`Error: ${err}`)
     res.json(action)
@@ -57,7 +61,7 @@ router.get("/actif", (req, res) => {
 })
 
 // Add new action
-router.post('/add', upload.single("image"), (req, res) => {
+router.post('/add', upload.single('image'), (req, res) => {
   // protected routes
   context = req.headers
   const { username } = checkAuth(context)
@@ -69,7 +73,7 @@ router.post('/add', upload.single("image"), (req, res) => {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
-    status: IS_PUBLISHED
+    status: IS_PUBLISHED,
   }
 
   let action = {
@@ -94,26 +98,27 @@ router.post('/add', upload.single("image"), (req, res) => {
 
   const newAction = new Actions(action)
 
-  newAction.save()
+  newAction
+    .save()
     .then(() => res.json('Nouvelle action ajoutée'))
-    .catch(err => res.status(400).json(`Error: ${err}`))
+    .catch((err) => res.status(400).json(`Error: ${err}`))
 })
 
 // Find action by id
 router.get('/:id', (req, res) => {
   Actions.findById(req.params.id)
-    .then(action => res.json(action))
-    .catch(err => res.status(400).json(`Error: ${err}`))
+    .then((action) => res.json(action))
+    .catch((err) => res.status(400).json(`Error: ${err}`))
 })
 
 // Find action by id and update
-router.put('/update/:id', upload.single("image"), (req, res) => {
+router.put('/update/:id', upload.single('image'), (req, res) => {
   // protected routes
   context = req.headers
   const { username } = checkAuth(context)
 
   Actions.findById(req.params.id)
-    .then(action => {
+    .then((action) => {
       const IS_EDITED = 'edited'
 
       let newUser = {
@@ -121,7 +126,7 @@ router.put('/update/:id', upload.single("image"), (req, res) => {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        status: IS_EDITED
+        status: IS_EDITED,
       }
 
       action.title = req.body.title
@@ -134,35 +139,35 @@ router.put('/update/:id', upload.single("image"), (req, res) => {
       action.users.push(newUser)
       action
         .save()
-        .then(() => res.status(200).json({ action: action, ok: "L'action est mise à jour" }))
-        .catch(err => res.status(400).json(`Error: ${err}`))
+        .then(() =>
+          res
+            .status(200)
+            .json({ action: action, ok: "L'action est mise à jour" })
+        )
+        .catch((err) => res.status(400).json(`Error: ${err}`))
     })
-    .catch(err => res.status(400).json(`Error: ${err}`))
+    .catch((err) => res.status(400).json(`Error: ${err}`))
 })
 
 // Find action by id and delete of db
 router.delete('/:id', (req, res) => {
-
   // protected routes
   context = req.headers
   const { username } = checkAuth(context)
-
 
   Actions.findByIdAndDelete(req.params.id)
     .then(() => res.json("L'action est supprimée"))
-    .catch(err => res.status(400).json(`Error: ${err}`))
+    .catch((err) => res.status(400).json(`Error: ${err}`))
 })
 
 // Find action by id and delete = true (reste en backup dans la db)
-router.put('/delete/:id', upload.single("image"), (req, res) => {
-
+router.put('/delete/:id', upload.single('image'), (req, res) => {
   // protected routes
   context = req.headers
   const { username } = checkAuth(context)
 
-
   Actions.findById(req.params.id)
-    .then(action => {
+    .then((action) => {
       const IS_DELETED = true
       const DELETED = 'deleted'
 
@@ -171,7 +176,7 @@ router.put('/delete/:id', upload.single("image"), (req, res) => {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        status: DELETED
+        status: DELETED,
       }
 
       action.status = DELETED
@@ -180,11 +185,12 @@ router.put('/delete/:id', upload.single("image"), (req, res) => {
       action.users.push(newUser)
       action
         .save()
-        .then(() => res.status(200).json({ action: action, ok: "L'action est supprimée" }))
-        .catch(err => res.status(400).json(`Error: ${err}`))
+        .then(() =>
+          res.status(200).json({ action: action, ok: "L'action est supprimée" })
+        )
+        .catch((err) => res.status(400).json(`Error: ${err}`))
     })
-    .catch(err => res.status(400).json(`Error: ${err}`))
+    .catch((err) => res.status(400).json(`Error: ${err}`))
 })
-
 
 module.exports = router
