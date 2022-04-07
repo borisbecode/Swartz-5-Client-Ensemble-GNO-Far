6d62160c44045f936bc65002b17e41867f74b9f1
borisbecode <boris.lorenzonetto@gmail.com>
2022-04-07 07:32:35 +0200

    parraine

--- server/routes/parraines.js
+++ server/routes/parraines.js
@@ -7,7 +7,7 @@ require('dotenv').config({ path: '../.env' })
 // definis le storage pour l'image
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, 'app/build/uploads/')
+    callback(null, 'app/public/uploads/')
   },
   filename: (req, file, callback) => {
     callback(null, Date.now() + file.originalname)
