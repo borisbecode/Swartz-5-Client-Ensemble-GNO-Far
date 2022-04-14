0b85e2a2973106928c8c4e19c3b3ecc40ad204e6
borisbecode <boris.lorenzonetto@gmail.com>
2022-04-14 11:59:00 +0200

    box

--- app/src/components/Child_pictures/settings.jsx
+++ app/src/components/Child_pictures/settings.jsx
@@ -1,10 +1,10 @@
-import { Box } from '@mui/material/node_modules/@mui/system'
+import Box from '@mui/material/Box'
 import React, { useState } from 'react'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import axios from 'axios'
 import TextField from '@mui/material/TextField'
-import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto'
 
 class Settings extends React.Component {
   state = {
@@ -62,10 +62,10 @@ class Settings extends React.Component {
           <Button
             onClick={() => this.fileInput.click()}
             variant="contained"
-            sx={{ bgcolor: 'secondary.main',color: 'white', mt: 5, mr: 3 }}
+            sx={{ bgcolor: 'secondary.main', color: 'white', mt: 5, mr: 3 }}
             startIcon={<AddAPhotoIcon />}
           >
-             Choisir une image
+            Choisir une image
           </Button>
           <Box sx={{ mt: 4 }}>
             {' '}
--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec)); */
   console.log(`Express is working on port ${port}`);
 }); */
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 41873;
 app.listen(PORT, () =>
   console.log(`Listening on PORT ${PORT}...🤓🤓🤓🤓🤓🤓🤓`)
 );
