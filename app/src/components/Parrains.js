c61f59eeb2690db4d7d9032f16bacef2a04096d3
borisbecode <boris.lorenzonetto@gmail.com>
2022-04-14 14:36:01 +0200

    parrain

--- app/src/components/Parrains.js
+++ app/src/components/Parrains.js
@@ -1,81 +1,127 @@
-import React from 'react';
-import Divider from '@mui/material/Divider';
-import Box from '@mui/material/Box';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import { ThemeProvider } from '@mui/material/styles';
-import { ThemeTitres } from '../theme/ThemeTitres';
+import React from 'react'
+import Divider from '@mui/material/Divider'
+import Box from '@mui/material/Box'
+import Avatar from '@mui/material/Avatar'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import { ThemeProvider } from '@mui/material/styles'
+import { ThemeTitres } from '../theme/ThemeTitres'
 
 function Parrains() {
   return (
-    <Box sx={{pt:5}}>
-        <ThemeProvider theme={ThemeTitres}>
-              <Typography variant="h1" sx={{fontSize:"1.5rem", mt:4}}>
-                  Nos parrains et leur témoignage
-              </Typography>
-          </ThemeProvider>
-        
-        <Divider sx={{my:4}}/>
+    <Box sx={{ pt: 5 }}>
+      <ThemeProvider theme={ThemeTitres}>
+        <Typography variant="h1" sx={{ fontSize: '1.5rem', mt: 4 }}>
+          Nos parrains et leur témoignage
+        </Typography>
+      </ThemeProvider>
 
-        <Box>
-            <Box  sx={{mb:5, display:"flex", flexDirection:{xs:"column", sm:"row"}, textAlign:{xs:"center", sm:"left"}, alignItems:{sx:"center", sm:"flex-start"}, justifyContent:"center"}}>
-                <Avatar
-                alt="Prénom Nom"
-                src="https://picsum.photos/id/122/800/800"
-                sx={{ width: 200, height: 200, mx:{xs:"auto"} }}
-                />
-                <Box sx={{ml:{sm:4}}}>
-                    <Typography
-                    variant="h3"
-                    color="primary.main"
-                    sx={{ mt: 4, fontSize: '1rem', fontWeight:"bold" }}>Prénom Nom</Typography>
-                    <Typography
-                    variant="body2" gutterBottom
-                    sx={{ mt: 2 }}>Nunc sollicitudin urna eu risus consequat, vel rhoncus leo facilisis. Maecenas facilisis, leo id hendrerit condimentum, massa nisi porttitor mi, in ornare est urna ac elit. Donec tellus lorem, venenatis vitae risus non, interdum lobortis nibh. Mauris magna felis, tempor eget viverra id, sagittis a nunc.
-                    </Typography>
-                </Box>
-            </Box>
+      <Divider sx={{ my: 4 }} />
 
-            <Box  sx={{mb:5, display:"flex", flexDirection:{xs:"column", sm:"row"}, textAlign:{xs:"center", sm:"left"}, alignItems:{sx:"center", sm:"flex-start"}, justifyContent:"center"}}>
-                <Avatar
-                alt="Prénom Nom"
-                src="https://picsum.photos/id/788/800/800"
-                sx={{ width: 200, height: 200, mx:{xs:"auto"} }}
-                />
-                <Box sx={{ml:{sm:4}}}>
-                    <Typography
-                    variant="h3"
-                    color="primary.main"
-                    sx={{ mt: 4, fontSize: '1rem', fontWeight:"bold" }}>Prénom Nom</Typography>
-                    <Typography
-                    variant="body2" gutterBottom
-                    sx={{ mt: 2 }}>Nulla sed pharetra ex. Proin commodo vulputate ante eget auctor. Sed blandit ipsum et sapien hendrerit, a imperdiet lorem accumsan. Maecenas scelerisque bibendum ante quis eleifend. Aenean et orci at risus pulvinar venenatis ac eu turpis. Etiam feugiat, erat et tempus faucibus, ligula lectus convallis lacus, nec pulvinar turpis ligula vel erat. Nulla tortor elit, porttitor eu vestibulum vel, facilisis ac metus. Vivamus convallis pellentesque eros, a malesuada nunc consectetur non. 
-                    </Typography>
-                </Box>
-            </Box>
-
-
-            <Box  sx={{mb:5, display:"flex", flexDirection:{xs:"column", sm:"row"}, textAlign:{xs:"center", sm:"left"}, alignItems:{sx:"center", sm:"flex-start"}, justifyContent:"center"}}>
-                <Avatar
-                alt="Prénom Nom"
-                src="https://picsum.photos/id/58/800/800"
-                sx={{ width: 200, height: 200, mx:{xs:"auto"} }}
-                />
-                <Box sx={{ml:{sm:4}}}>
-                    <Typography
-                    variant="h3"
-                    color="primary.main"
-                    sx={{ mt: 4, fontSize: '1rem', fontWeight:"bold" }}>Prénom Nom</Typography>
-                    <Typography
-                    variant="body2" gutterBottom
-                    sx={{ mt: 2 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vestibulum varius vestibulum. Morbi at viverra lectus, vitae aliquet neque. Phasellus eleifend libero nec felis euismod, accumsan facilisis massa sodales. Sed odio nisl, porttitor quis imperdiet vel, pulvinar in libero. Suspendisse dictum, neque ac sodales tempor, nulla felis pretium ex, at molestie enim augue quis nibh. Nam non tristique erat. Vestibulum id volutpat est. Nullam lacinia lectus in sem consectetur condimentum. Duis placerat dui ut tincidunt tempus. Curabitur vel rutrum est. Maecenas dolor dolor, mattis mollis justo in, convallis vulputate ipsum. Aenean blandit aliquam tristique. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam vestibulum egestas vulputate.
-                    </Typography>
-                </Box>
-            </Box>
-
+      <Box>
+        <Box
+          sx={{
+            mb: 5,
+            display: 'flex',
+            flexDirection: { xs: 'column', sm: 'row' },
+            textAlign: { xs: 'center', sm: 'left' },
+            alignItems: { sx: 'center', sm: 'flex-start' },
+            justifyContent: 'center',
+          }}
+        >
+          <Avatar
+            alt="Amandine Fourdin"
+            src="https://zupimages.net/up/22/15/xgox.jpg"
+            sx={{ width: 200, height: 200, mx: { xs: 'auto' } }}
+          />
+          <Box sx={{ ml: { sm: 4 } }}>
+            <Typography
+              variant="h3"
+              color="primary.main"
+              sx={{ mt: 4, fontSize: '1rem', fontWeight: 'bold' }}
+            >
+              Amandine Fourdin
+            </Typography>
+            <Typography variant="body2" gutterBottom sx={{ mt: 2 }}>
+              N'hésitez pas à venir rejoindre la grande famille des parrains et
+              marraines, ce n'est que du bonheur de pouvoir aider ces petits et
+              leur famille grâce à une équipe formidable dans une association
+              transparente et honnête.
+            </Typography>
+          </Box>
         </Box>
 
+        <Box
+          sx={{
+            mb: 5,
+            display: 'flex',
+            flexDirection: { xs: 'column', sm: 'row' },
+            textAlign: { xs: 'center', sm: 'left' },
+            alignItems: { sx: 'center', sm: 'flex-start' },
+            justifyContent: 'center',
+          }}
+        >
+          <Avatar
+            alt="Colette Vanderwaele"
+            src="https://zupimages.net/up/22/15/1fwa.jpg"
+            sx={{ width: 200, height: 200, mx: { xs: 'auto' } }}
+          />
+          <Box sx={{ ml: { sm: 4 } }}>
+            <Typography
+              variant="h3"
+              color="primary.main"
+              sx={{ mt: 4, fontSize: '1rem', fontWeight: 'bold' }}
+            >
+              Colette Vanderwaele
+            </Typography>
+            <Typography variant="body2" gutterBottom sx={{ mt: 2 }}>
+              Nouvelle membre de cette association, j'étais un peu réticente au
+              départ, mais je ne regrette rien, bien que je n'ai pas voulu
+              choisir mon filleul, j'ai entièrement fait confiance à Annie
+              Tilmant , et c'est elle qui m'a permis d'améliorer la vie de
+              Pierre, et ce n'est pas l'association qui demande quoi que ce
+              soit, mais c'est moi qui veut aider Pierre , ils restent très
+              discrets, mais les comptes, eux, sont limpides, encore un tout
+              grand merci à toute l'équipe et de faire de moi une marraine
+              heureuse
+            </Typography>
+          </Box>
+        </Box>
+
+        <Box
+          sx={{
+            mb: 5,
+            display: 'flex',
+            flexDirection: { xs: 'column', sm: 'row' },
+            textAlign: { xs: 'center', sm: 'left' },
+            alignItems: { sx: 'center', sm: 'flex-start' },
+            justifyContent: 'center',
+          }}
+        >
+          <Avatar
+            alt="Valeria Lai-Mayence"
+            src="https://zupimages.net/up/22/15/nxtb.jpg"
+            sx={{ width: 200, height: 200, mx: { xs: 'auto' } }}
+          />
+          <Box sx={{ ml: { sm: 4 } }}>
+            <Typography
+              variant="h3"
+              color="primary.main"
+              sx={{ mt: 4, fontSize: '1rem', fontWeight: 'bold' }}
+            >
+              Valeria Lai-Mayence
+            </Typography>
+            <Typography variant="body2" gutterBottom sx={{ mt: 2 }}>
+              Je parraine un petite sénégalaise depuis quelques années déjà et
+              je ne regrette pas avoir franchi le pas. L'asbl nous communique
+              les différents projets en toute transparence et nous fait des
+              retours de nos petits protégés. Je fais entièrement confiance en
+              l'équipe et tout particulièrement à Annie et Jean Luc pour leur
+              investissement et leur implication.
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   )
 }
